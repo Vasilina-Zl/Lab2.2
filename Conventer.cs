b9590e392a45a78711ead07f4d654f13180bb40a
Vasilina-Zl <terisharkis@gmail.com>
2022-02-15 11:08:54 +0200

    first commit

---
+++ Conventer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2._2
+{
+    internal class Conventer
+    {
+        public double purchaseUSD;
+        public double saleUSD;
+
+        public double purchaseEUR;
+        public double saleEUR;
+
+        public double purchaseRUB;
+        public double saleRUB;
+
+        public double compareSum = 0;
+        public string changeFromStr, changeToStr;
+        public string cycleCondition = "";
+        public double changeSum;
+
+
+        public double Calculate(string cfs, double changeSum)
+        {
+            if (cfs == "1")
+            {
+               return If1(changeToStr, changeSum);
+            }
+            else if (cfs == "2")
+            {
+                compareSum = changeSum * saleUSD;
+                return compareSum;
+            }
+            else if (cfs == "3")
+            {
+                compareSum = changeSum * saleEUR;
+                return compareSum;
+            }
+            else if (cfs == "4")
+            {
+                compareSum = changeSum * saleRUB;
+                return compareSum;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double If1(string cts, double cs) 
+        {
+            Console.Write("Яку валюту бажаєте отримати?\n" +
+                                   "1-долари, 2-євро, 3-рублi: ");
+            changeToStr = Console.ReadLine();
+
+
+            if (changeToStr == "1")
+            {
+                compareSum = cs / purchaseUSD;
+                return compareSum;
+            }
+            else if (changeToStr == "2")
+            {
+                compareSum = cs / purchaseEUR;
+                return compareSum;
+            }
+            else if (changeToStr == "3")
+            {
+                compareSum = cs / purchaseRUB;
+                return compareSum;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conventer stconventer = new Conventer();
+
+            stconventer.purchaseUSD = 27.8500;
+            stconventer.saleUSD = 28.0500;
+
+            stconventer.purchaseEUR = 31.7150;
+            stconventer.saleEUR = 32.0500;
+
+            stconventer.purchaseRUB = 0.3570;
+            stconventer.saleRUB = 0.3720;
+
+            double compareSum = 0;
+            string changeFromStr, changeToStr;
+            string cycleCondition = "";
+
+
+
+            while (cycleCondition != "exit")
+            {
+                Console.Clear();
+                Console.WriteLine("Конвертер валют\n");
+
+
+
+
+                Console.SetCursorPosition(13, 2);
+                Console.WriteLine("Поточний курс валют:\n");
+                Console.WriteLine($"Продати USD: {stconventer.saleUSD} RUB, "
+                    + $"Купити USD: {stconventer.purchaseUSD} RUB\n"
+                    + $"Продати EUR: {stconventer.saleEUR} RUB, "
+                    + $"Купити EUR: {stconventer.purchaseEUR} RUB\n"
+                    + $"Продати RUB: {stconventer.saleRUB} RUB, "
+                    + $"Купити RUB: {stconventer.purchaseRUB} RUB\n");
+
+
+                Console.Write("Яку валюту бажаєте помiняти?\n" +
+                         "1-гривнi, 2-долари, 3-євро, 4-рублi: ");
+                changeFromStr = Console.ReadLine();
+
+                Console.Write("Введiть суму для обмiну: ");
+                double changeSum = Convert.ToDouble(Console.ReadLine());
+
+                
+                
+                stconventer.Calculate(changeFromStr, changeSum);
+               
+
+                Console.Clear();
+                Console.WriteLine($"Обмiн зроблено.\n" +
+                    $"У Вас {Math.Round(stconventer.compareSum, 2)}");
+
+                Console.WriteLine("Для продовження натиснiть Enter,\n" +
+                    "для виходу з програми введiть exit i натиснiть Enter");
+
+                cycleCondition = Console.ReadLine();
+            }
+        }
+    }
+}
+
